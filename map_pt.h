648c405ff5ee5bbbd8ecab3dd1e1e4510d2dbdb4
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-12-18 08:37:25 +0100

    add patricia tree implementation

--- map_pt.h
+++ map_pt.h
@@ -5,25 +5,122 @@
 #include <utility>
 #include <iterator>
 #include <list>
+#include <climits>
 
+const unsigned LENGTH_PT = 3;
+unsigned COUNTER_PT = 0;
 template<class K, class D>
-class map_rb {
+class map_pt {
 public:
     typedef std::pair<const K, D> value_type;
 
-
 private:
+    static bool left(const K& key, unsigned bitPosition){
+        return ( key & (1 << bitPosition) ) == 0;
+    }
     // Node class
     struct Node {
-        Node(const K &key, const D &value) : m_value(key, value) {}
+        Node(const Node& node) = delete;
+        Node& operator=(const Node& node) = delete;
+        Node(const K &key, const D &value, unsigned bitPos, Node* succ) : m_value(key, value), m_bitPos(bitPos) {
+            bool isLeftOrRight = left(m_value.first, bitPos);
+            m_pLeft = isLeftOrRight ? this : succ;
+            m_pRight = isLeftOrRight ? succ : this;
+        }
+
+        Node(const K &key, const D &value, unsigned bitPos) : Node(key, value, bitPos, nullptr){};
 
         ~Node() {
             delete m_pLeft;
             delete m_pRight;
         }
+
+        friend std::ostream &operator<<(std::ostream &os, const Node &crArg) {
+
+            if (crArg.m_pLeft) {
+                COUNTER_PT = 0;
+                os << *crArg.m_pLeft;
+
+            }
+            for (int i = 0; i < COUNTER_PT; ++i)
+                std::cout << '\t';
+            os << "( key : " << crArg.m_value.first << " | value: " << crArg.m_value.second << " | color: "
+               << crArg.m_bitPos << ")" << std::endl;
+            ++COUNTER_PT;
+
+            if (crArg.m_pRight) {
+                COUNTER_PT = 0;
+                os << *crArg.m_pRight;
+            }
+
+            return os;
+        }
         value_type m_value;
+        unsigned m_bitPos{};
         Node *m_pLeft = nullptr;
         Node *m_pRight = nullptr;
+    };
+
+    struct NodeHandler {
+
+        explicit NodeHandler(Node *&root) : handlerRoot(root) {
+            for (unsigned i = LENGTH_PT - 1; i > 0; --i) {
+                m_Nodes[i] = nullptr;
+            }
+            m_Nodes[NODE] = root;
+        }
+
+        void down(bool direction) {
+            for (unsigned i = LENGTH_PT - 1; i > 0; --i)
+                m_Nodes[i] = m_Nodes[i - 1]; // [2] = DAD | DAD = NODE
+            m_Nodes[NODE] = direction ? node(DAD)->m_pRight : node(DAD)->m_pLeft; // TODO check if the direction should be from the father or the current node
+        }
+
+        void search(const K & key, int maxBitValue){
+            int lastBitPos = -1;
+            while (!isNull() && lastBitPos < node(NODE)->m_bitPos && maxBitValue > node(NODE)->m_bitPos){
+                lastBitPos = node(NODE)->m_bitPos;
+                down(left(key, lastBitPos)); // if the lastBitPos of the key is set to { 1 -> then go right direction  | 0 -> then go left direction }
+            }
+        }
+
+        void search(const K & key) {
+            search(key, INT_MAX);
+        }
+
+
+        void set(Node *nodeToSet, unsigned kind) {
+            if (!node(kind + 1))
+                handlerRoot = nodeToSet;
+            else if (nodeToSet->m_value.first < node(kind + 1)->m_value.first)
+                node(kind + 1)->m_pLeft = nodeToSet;
+            else
+                node(kind + 1)->m_pRight = nodeToSet;
+            m_Nodes[kind] = nodeToSet;
+        }
+
+        bool isNull() {
+            return m_Nodes[NODE] == nullptr;
+        }
+
+        Node *node(unsigned kind) {
+            return m_Nodes[kind];
+        }
+
+        NodeHandler& operator=(const NodeHandler& crArg){
+            if(this != &crArg){
+                handlerRoot = crArg.handlerRoot;
+                for(int i = 0; i < LENGTH_PT;++i){
+                    m_Nodes[i] = crArg.m_Nodes[i];
+                }
+            }
+            return *this;
+        }
+
+        Node *m_Nodes[LENGTH_PT];
+        const unsigned NODE = 0;
+        const unsigned DAD = 1;
+        Node *&handlerRoot = nullptr;
     };
 
 public:
@@ -70,7 +167,6 @@ public:
             return tmp;
         }
 
-        //value_type& operator*(){
         typename iterator::value_type &operator*() {
             return m_Nodes2Visit.back()->m_value;
         }
@@ -95,83 +191,65 @@ public:
 
     };
 
-    map_rb()= default;
+    map_pt()= default;
 
-    ~map_rb()
+    ~map_pt()
     {
         delete m_pRoot;
     }
 
-    map_rb(const map_rb<K, D> &crArg)  // TODO: deleting copy constructor if not used
-    {
-        if (crArg.m_pRoot)
-            m_pRoot = new Node(*crArg.m_pRoot);
-    }
+    map_pt(const map_pt<K, D> &crArg)  = delete;
 
-    map_rb &operator=(const map_rb<K, D> &crArg)  // TODO: deleting assign operator if not used
-    {
-        if (&crArg != this)
-        {
-            delete m_pRoot;
-            m_pRoot = nullptr;
-            if (crArg.m_pRoot)
-                m_pRoot = new Node(*crArg.m_pRoot);
-        }
-        return *this;
-    }
+    map_pt &operator=(const map_pt<K, D> &crArg) = delete;
 
 
-    iterator find(const K& key) const
+    iterator find(const K& key)
     {
-        Node *pTmp = m_pRoot;
-        while (pTmp != nullptr)
-        {
-            if (key < pTmp->m_value.first)
-                pTmp = pTmp->m_pLeft;
-            else if (pTmp->m_value.first < key)
-                pTmp = pTmp->m_pRight;
-            else
-                // has been found
-                return iterator(pTmp);
-        }
-        return end();
+        NodeHandler handler(m_pRoot);
+        handler.search(key);
+        if(!handler.isNull() && handler.node(handler.NODE)->m_value.first == key)
+            return iterator(handler.node(handler.NODE));
+        else
+            return end();
     }
 
 
 
     std::pair<iterator, bool> insert(const value_type& value)
     {
-        return insertRec(m_pRoot, value.first, value.second);
+        auto keyToAdd = value.first;
+        NodeHandler h(m_pRoot);
+        h.search(keyToAdd);
+        unsigned index = 0;
+        if(h.isNull()) {
+            if (h.node(h.DAD))
+                index = h.node(h.DAD)->m_bitPos + 1;
+        }else if(h.node(h.NODE)->m_value.first != keyToAdd){
+            while(left(h.node(h.NODE)->m_value.first, index) == left(keyToAdd, index))
+                ++index;
+        }else
+            return std::pair<iterator, bool>(end(), false);
+
+        h = NodeHandler(m_pRoot);
+        Node* newNode = new Node(keyToAdd, value.second, index, h.node(h.NODE));
+        h.search(keyToAdd, index);
+        h.set(newNode, h.NODE);
+        return std::pair<iterator, bool>(newNode, true);
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const map_rb<K, D> &crArg)
-    {
+    friend std::ostream &operator<<(std::ostream &os, const map_pt &crArg) {
         if (crArg.m_pRoot)
             os << *crArg.m_pRoot;
         return os;
     }
 
 private:
-    std::pair<iterator, bool> insertRec(Node *&pTmp, const K &pKey, const D &pData)
-    {
-        if (!pTmp) {
-            pTmp = new Node(pKey, pData);
-            return std::pair<iterator, bool>(iterator(pTmp), true);
-        }
-        else
-        {
-            if (pKey < pTmp->m_value.first)
-                return insertRec(pTmp->m_pLeft, pKey, pData);
-            else if (pTmp->m_value.first < pKey)
-                return insertRec(pTmp->m_pRight, pKey, pData);
-        }
-    }
-
     Node *m_pRoot = nullptr;
+
 public:
     iterator begin() { return iterator(m_pRoot); }
     iterator end() const { return iterator(nullptr); }
-//    const_iterator  cbegin() const { return const_iterator(m_prRoot); } // todo uncomment this part later
+//    const_iterator  cbegin() const { return const_iterator(handlerRoot); } // todo uncomment this part later
 //    const_iterator cend() const { return const_iterator(nullptr); } // todo uncomment this part later
 };
 
