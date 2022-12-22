774e93d82386f87e662111362d4ce88f2c269433
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-12-22 21:18:34 +0100

    add new compiler flags to makefile

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-//#include "map_rb.h"
-#include "map_pt.h"
+#include <map>
+#include "map_rb.h"
+//#include "map_pt.h"
 
 using namespace std;
 
 
 int main() {
 
-    map_pt<char> mymap;
-//    map_rb<unsigned ,char> mymap;
-    auto var1 = std::pair<unsigned , char>(0, 'a');
-    auto var2 = std::pair<unsigned, char>(1, 'b');
-    auto var3 = std::pair<unsigned, char>(2, 'c');
-    auto var4 = std::pair<unsigned, char>(3, 'd');
-    auto var7 = std::pair<unsigned, char>(34, 'd');
-    auto var5 = std::pair<unsigned, char>(17, 'd');
-    auto var6 = std::pair<unsigned, char>(24, 'd');
+//    map_pt<char> mymap;
+    map_rb<unsigned, char> mymap;
+//    map<unsigned ,char> mymap;
+    auto var1 = std::pair<unsigned, char>(12, 'a');
+    auto var2 = std::pair<unsigned, char>(9, 'b');
+    auto var3 = std::pair<unsigned, char>(6, 'c');
+    auto var4 = std::pair<unsigned, char>(4, 'd');
+    auto var7 = std::pair<unsigned, char>(34, 'e');
+    auto var5 = std::pair<unsigned, char>(58, 'f');
+    auto var6 = std::pair<unsigned, char>(22, 'g');
 
     auto res = mymap.insert(var1);
     auto res2 = mymap.insert(var2);
@@ -26,41 +28,22 @@ int main() {
     auto res7 = mymap.insert(var7);
 
 
-    cout << "-----------------" << endl;
+    auto result = mymap.find(4);
+    if (result == mymap.end())
+        cout << "find not work" << endl;
+    else
+        cout << result->first << " : " << result->second << endl;
+    ++result;
+    cout << result->first << " : " << result->second << endl;
+    ++result;
+    cout << result->first << " : " << result->second << endl;
+    ++result;
+    cout << result->first << " : " << result->second << endl;
+    ++result;
+    cout << result->first << " : " << result->second << endl;
+    ++result;
+    cout << result->first << " : " << result->second << endl;
+    ++result;
+    cout << result->first << " : " << result->second << endl;
 
-    cout << "key 0: " << res.first->first << "\nchar 0: " << res.first->second << "\nresult insert 0 : " << res.second << std::endl;
-    cout << "key 1: " << res2.first->first << "\nchar 1: " << res2.first->second << "\nresult insert 1 : " << res2.second << std::endl;
-    cout << "key 2: " << res3.first->first << "\nchar 2: " << res3.first->second << "\nresult insert 2 : " << res3.second << std::endl;
-    cout << "key 3: " << res3.first->first << "\nchar 2: " << res4.first->second << "\nresult insert 2 : " << res3.second << std::endl;
-    cout << "key 4: " << res3.first->first << "\nchar 2: " << res5.first->second << "\nresult insert 2 : " << res3.second << std::endl;
-    cout << "key 5: " << res3.first->first << "\nchar 2: " << res6.first->second << "\nresult insert 2 : " << res3.second << std::endl;
-    cout << "key 6: " << res3.first->first << "\nchar 2: " << res7.first->second << "\nresult insert 2 : " << res3.second << std::endl;
-
-    cout << "-----------------" << endl;
-
-//    auto result = mymap.find(24);
-//    if (result == mymap.end())
-//        cout << "find not work" << endl;
-//    else
-//        cout << result->first << " : " << result->second << endl;
-
-//    cout << mymap << endl;
-
-
-    //    for(auto i = mymap.begin(); i != mymap.end(); i++){
-//        cout << "key : " <<  i->first << "\nchar : " << i->second << endl;
-//        cout << "+++++++++" << endl;
-//    }
-
-//    int a = 3;
-//    for (int i = 0; i < 4; ++i){
-//        bool f = ( a & ( 1 << i) ) != 0;
-//        cout << f << endl;
-//    }
-
-//char c = '\t';
-//int count = 2;
-//for(int i = 0; i < count; ++i)
-//    cout << c;
-//cout << "teste tab" << endl;
 }
